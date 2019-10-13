7f84e000a3d58c479d7efa8355ec39b80786b721
angelaaaaliu <liu.angela.1@gmail.com>
2019-10-12 20:20:34 -0700

    meal class

--- Order.java
+++ Order.java
@@ -1,19 +1,14 @@
 public class Order {
     private Meal meal;
     private int quantity;
-    private int pickUpHour;
-    private int pickUpMinute;
-    private boolean amPM;
+    private String time;
     private boolean addToCart;
     private boolean addToCalendar;
 
-    public Order(Meal meal, int quantity, int pickUpHour, int pickUpMinute,
-                 boolean amPM, boolean addToCart, boolean addToCalendar) {
+    public Order(Meal meal, int quantity, String time, boolean addToCart, boolean addToCalendar) {
         this.meal = meal;
         this.quantity = quantity;
-        this.pickUpHour = pickUpHour;
-        this.pickUpMinute = pickUpMinute;
-        this.amPM = amPM;
+        this.time = time;
         this.addToCart = addToCart;
         this.addToCalendar = addToCalendar;
 
@@ -22,5 +17,23 @@ public class Order {
         }
     }
 
+    public int getQuantity() {
+        return quantity;
+    }
 
+    public String getTime() {
+        return time;
+    }
+
+    public boolean isAddToCart() {
+        return addToCart;
+    }
+
+    public boolean isAddToCalendar() {
+        return addToCalendar;
+    }
+
+    public double orderTotal() {
+        return Math.round((meal.price * quantity) * 100.0) / 100.0;
+    }
 }
