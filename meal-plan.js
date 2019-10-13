d8aed5ccd82b384f45c68d392065ae09a9ce19be
lumk3 <56491375+lumk3@users.noreply.github.com>
2019-10-12 20:42:44 -0700

    side nav bar works

--- meal-plan.js
+++ meal-plan.js
@@ -1,8 +1,34 @@
-/* Animated side menu navigation */
-function openNav() {
-  document.getElementById("mySidenav").style.width = "250px";
+(function() {
+    console.log("Hey");
+    window.addEventListener("load", init);
+    
+    function init() {
+        document.getElementById("open-sidenav").addEventListener("click", expand);
+        document.getElementById("close-button").addEventListener("click", collapse);
+    }
+    
+    console.log("hey2");
+
+// Animated side menu navigation
+function expand() {
+    document.getElementById("my-sidenav").style.width = "25%";
+    console.log("Yay");
 }
 
-function closeNav() {
-  document.getElementById("mySidenav").style.width = "0";
+function collapse() {
+    document.getElementById("my-sidenav").style.width = "0%";
 }
+/*
+// checkout button on home page
+function checkout() {
+  document.getElementById("checkoutButton").addEventListener("click", checkoutClick);
+}
+
+function checkoutClick() {
+  // TODO: Until Pao defines checkout page id's
+  // hide all the other stuff
+  document.getElementById("???").style.display = "none";
+  // make visible the checkout divs
+  document.getElementById("???").style.display = "block";
+}*/
+})();
